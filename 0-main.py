25db1d32e324a1503ba858533959bcb3f77b589a
Long-belmont-RW <dakogolgabriel@gmail.com>
2025-07-04 09:15:46 +0100

    clean up duplicate files and keep the neccessary files

--- 0-main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Main file to test wait_random function."""
-
-import asyncio
-wait_random = __import__('0-basic_async_syntax').wait_random
-
-print(asyncio.run(wait_random()))
-print(asyncio.run(wait_random(5)))
-print(asyncio.run(wait_random(15)))
