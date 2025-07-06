a88a195772203d8f0b9e5a3c44d7dacb1a30fa4a
Long-belmont-RW <dakogolgabriel@gmail.com>
2025-07-07 00:51:17 +0100

    async comprehension

--- python_async_function/0-async_generator.py
+++ python_async_function/0-async_generator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import asyncio
 import random
+from typing import AsyncGenerator
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[int, None]:
     """A corouting that loops 10 times, each time waiting 1 seco    nd and yeilding a random number between 0 and 10"""
 
     for i in range(10):
